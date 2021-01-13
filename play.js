2e2727e1b0dba1b90902391eea0984063252e00c
anilkpatel <anil.patel@mail.mcgill.ca>
2021-01-13 18:47:37 +0000

    Solution for module.export.js

---
+++ play.js
@@ -0,0 +1,11 @@
+//Lighthouse Lab
+//Week 2, Day 3
+//Multiplayer Snake Game
+//Anil Patel, pair programmed with Desk 00
+
+//Refactored code
+const { connect } = require('./client');
+console.log('Connecting ...');
+connect();
+
+module.exports = connect;
