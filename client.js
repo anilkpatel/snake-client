aa04d934e37733b375a35e3b99fac509e92afd92
anilkpatel <anil.patel@mail.mcgill.ca>
2021-01-14 00:40:27 +0000

    Added stdin in play.js

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 //Lighthouse Lab
 //Week 2, Day 3
 //Multiplayer Snake Game
-//Anil Patel, pair programmed with Desk 00
+//Anil Patel, pair programmed with Arlet Vasquez
 
 const net = require('net');
 
