bc8e8a69aa587091d0160efecfdce0a46bc71f29
anilkpatel <anil.patel@mail.mcgill.ca>
2021-01-14 00:41:10 +0000

    Added stdin in play.js

--- play.js
+++ play.js
@@ -1,11 +1,45 @@
 //Lighthouse Lab
 //Week 2, Day 3
 //Multiplayer Snake Game
-//Anil Patel, pair programmed with Desk 00
+//Anil Patel, pair programmed with Arlet Vasquez
 
-//Refactored code
+/*Refactored orginal code
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+*/
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+//FUNCTION stdin 
+// Event Handler
+// Register the Event with setupInput function above, BEFORE returning stdin object
+// 'Event Listener' such that when data called, event exist
+// Register/Capture the data and call the handle  
+
+stdin.on("data" , handleUserInput) 
+
+return stdin;
+}; 
+
+//FUNCTION handleUserInput 
+//Registered as "data" callback handler for Function stdin. 
+
+const handleUserInput = function (data) {
+if ('\u0003' === data) { // 
+  process.exit();
+  }
+};
+
+
+
 
 //module.exports = connect; not needed
